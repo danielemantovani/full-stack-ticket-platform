742a3dacd3830031c8827e597c9cc96e8a92d6bd
danielemantovani <daniele.mantovani124@gmail.com>
2024-11-22 11:26:04 +0100

    feat: seed catogieres table

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -5,14 +5,20 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Category;
 
 class CategoriesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //
+        // Genera 5 categorie fittizie
+        for ($i = 0; $i < 5; $i++) {
+            $newCategory = new Category();
+            $newCategory->name = $faker->word;
+            $newCategory->save();
+        }
     }
 }
