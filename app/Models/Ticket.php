90bfb731b4f629227b3bae736d357b99fcb8b8ad
danielemantovani <daniele.mantovani124@gmail.com>
2024-11-22 11:25:47 +0100

    feat: seed ticket table

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -11,13 +11,13 @@ class Ticket extends Model
 
     protected $fillable = ['title', 'description', 'status', 'operator_id', 'category_id'];
 
-    // Relazione con l'operatore (un ticket appartiene a un operatore)
+    // Relazione con l'operatore (un ticket appartiene a un operatore, molti ticket possono essere assegnati ad un operatore)
     public function operator()
     {
         return $this->belongsTo(Operator::class);
     }
 
-    // Relazione con la categoria (un ticket appartiene a una categoria)
+    // Relazione con la categoria (un ticket appartiene a una categoria, molti ticket posso avere la stessa categoria)
     public function category()
     {
         return $this->belongsTo(Category::class);
--- database/seeders/TicketsTableSeeder.php
+++ database/seeders/TicketsTableSeeder.php
@@ -4,14 +4,27 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Ticket;
+use App\Models\Operator;
+use App\Models\Category;
+use Faker\Generator as Faker;
 
 class TicketsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker)
     {
-        //
+        // Genera 15 ticket fittizi
+        for ($i = 0; $i < 15; $i++) {
+            $newTicket = new Ticket();
+            $newTicket->title = $faker->sentence; 
+            $newTicket->description = $faker->text;
+            $newTicket->status = $faker->randomElement(['ASSIGNED', 'IN PROGRESS', 'CLOSED']); // Stato del ticket
+            $newTicket->operator_id = Operator::inRandomOrder()->first()->id; // Assegna un operatore casuale
+            $newTicket->category_id = Category::inRandomOrder()->first()->id; // Assegna una categoria casuale
+            $newTicket->save();
+        }
     }
 }
